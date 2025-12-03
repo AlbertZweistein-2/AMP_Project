b66220a0e1a38766859cc5cafd64fedfa9850582
moritz techen <moritz.techen@gmail.com>
2025-12-03 09:53:05 +0100

    init and destroy

--- project_skeleton/src/ex1.cpp
+++ project_skeleton/src/ex1.cpp
@@ -14,6 +14,24 @@ struct queue {
     queue() : head(nullptr), tail(nullptr) {};
 };
 
+void init_queue(struct queue* Q) {
+    Q->head = nullptr;
+    Q->tail = nullptr;
+}
+
+void destroy_queue(struct queue* Q) {
+    struct node* current = Q->head;
+    struct node* next;
+
+    while (current != nullptr) {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+    Q->head = nullptr;
+    Q->tail = nullptr;
+}
+
 void enq(value_t v, struct queue Q);
 
 int deq(value_t *v, struct queue Q);
