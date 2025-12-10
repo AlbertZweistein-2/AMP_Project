d90b8f42a11373f52a874fc7a0998546e05f26c1
moritz techen <moritz.techen@gmail.com>
2025-12-10 11:58:05 +0100

    Redo Ex1 with C Language
    Co-authored-by: AlbertZweistein <AlbertZweistein-2@users.noreply.github.com>
    Co-authored-by: cathbl08 <cathbl08@users.noreply.github.com>

---
+++ project_skeleton/src/Ex1.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+
+typedef int value_t;
+
+typedef struct node {
+    value_t data;
+    struct node* next;
+} node_t;
+
+node_t* make_node(value_t v){
+    node_t* new_node = malloc(sizeof(node_t));
+    new_node->data = v;
+    new_node->next = NULL;
+    return new_node;
+}
+
+typedef struct freelist {
+    node_t* head;
+} freelist_t;
+
+typedef struct queue {
+    node_t* head;
+    node_t* tail;
+    freelist_t* free_lists;
+    int num_threads;
+} queue_t;
+
+void init_queue(queue_t* Q, int num_threads){
+    Q->head = make_node(0);
+    Q->tail = Q->head;
+    Q->free_lists = calloc(num_threads, sizeof(freelist_t));
+    Q->num_threads = num_threads;
+    return;
+}
+
+void destroy_queue(queue_t* Q){
+    node_t* next;
+
+    /* free main queue nodes */
+    while (Q->head != NULL) {
+        next = Q->head->next;
+        free(Q->head);
+        Q->head = next;
+    }
+
+    /* free nodes stored in each thread-local free list */
+    if (Q->free_lists) {
+        for (int i = 0; i < Q->num_threads; ++i) {
+            node_t* n = Q->free_lists[i].head;
+            while (n != NULL) {
+                node_t* tmp = n->next;
+                free(n);
+                n = tmp;
+            }
+        }
+        free(Q->free_lists);
+        Q->free_lists = NULL;
+        Q->num_threads = 0;
+    }
+
+    return;
+}
+
+// pull from local free list -> use in enqueue
+node_t* upcylce_node(queue_t* q, int tid) {
+    node_t* n = q->free_lists[tid].head;
+    if (n != NULL) {
+        q->free_lists[tid].head = n->next;
+        return n;
+    }
+    return malloc(sizeof(node_t));
+}
+
+// push in local free list -> use in dequeue
+void recycle_node(queue_t* Q, int tid, node_t* node) {
+    node->next = Q->free_lists[tid].head;
+    Q->free_lists[tid].head = node;
+}
+
+int enq(value_t v, queue_t* Q, int thread_id){
+    node_t* new_node = upcylce_node(Q, thread_id);
+    if(!new_node) return 0;
+    new_node->data = v;
+    new_node->next = NULL;
+    Q->tail->next = new_node;
+    Q->tail = new_node;
+    return 1;
+}
+
+int deq(value_t *v, queue_t* Q, int thread_id){
+    if(!Q->head->next) return 0;
+    node_t* sentinel = Q->head;
+    *v = sentinel->next->data;
+    Q->head = sentinel->next;
+    if(Q->tail == sentinel->next){
+        Q->tail = Q->head;
+    }
+    recycle_node(Q, thread_id, sentinel);
+    return 1;
+}
